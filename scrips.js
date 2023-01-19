88bb58a467f40da96ac22ec741448cfee93b99cf
NikiSukiasyan <justplayingcsgo@gmail.com>
2023-01-19 21:58:04 +0400

    added js

---
+++ scrips.js
@@ -0,0 +1,24 @@
+let user = "niki";
+let surname = "sukiasyan"
+let age = 17;
+let isMale = true;
+let car = null;
+console.log(user + " " + surname)
+console.log(`${user} ${surname}`)
+
+let user2 = "andria";
+let surname2 = undefined;
+let age2 = 13;
+let isMale2 = true;
+console.log(age+age2)
+console.log(age*age2)
+console.log(age-age2)
+console.log(age/age2)
+
+let width = 5;
+let length = 7;
+console.log(width*length)
+
+
+
+
